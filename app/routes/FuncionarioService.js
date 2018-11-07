f3db0588687f4e7c212161981be873a6d7fb2e9a
WESLEY ALVES DA SILVA <17170129@CFP127ALUNO.LOCAL>
2018-11-07 16:55:47 -0200

    Adaptações para o black mode

--- app/routes/FuncionarioService.js
+++ app/routes/FuncionarioService.js
@@ -1,7 +1,6 @@
 module.exports = (app) => {
     app.post("/funcionario/login", (req, res) => {
         const dao = new app.database.FuncionarioDAO(app);
-        console.log(req.body);
         dao.login(req.body.matricula, req.body.senha, (err, result) => {
             if (err) {
                 res.status(500);
