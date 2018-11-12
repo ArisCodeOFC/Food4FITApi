fa54cbda60cf1a0af67cd67de3512aff9c0a1d9c
WESLEY ALVES DA SILVA <17170129@CFP127ALUNO.LOCAL>
2018-11-12 16:52:08 -0200

    Adicionado mais dados no login de usuário

--- app/database/UsuarioDAO.js
+++ app/database/UsuarioDAO.js
@@ -7,7 +7,7 @@ class UsuarioDAO {
     
     login(email, senha = "", callback) {
         const hash = crypto.createHash("md5").update(senha).digest("hex");
-        this.connection.query("SELECT id, nome, sobrenome, email FROM tbl_usuario WHERE email = ? AND senha = ?", [email, hash], callback);
+        this.connection.query("SELECT id, nome, sobrenome, email, avatar, rg, cpf, data_nascimento AS dataNascimento, genero, telefone, celular FROM tbl_usuario WHERE email = ? AND senha = ?", [email, hash], callback);
     }
 }
 
