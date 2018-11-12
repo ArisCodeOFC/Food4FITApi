a483122115ddb51f15284ad8ea5fbe855ed79aa8
WESLEY ALVES DA SILVA <17170129@CFP127ALUNO.LOCAL>
2018-11-12 15:00:13 -0200

    Mapeamento do endereço do funcionário para objeto

--- app/database/FuncionarioDAO.js
+++ app/database/FuncionarioDAO.js
@@ -11,11 +11,17 @@ class FuncionarioDAO {
     }
     
     selecionar(id, callback) {
-        this.connection.query("SELECT f.id, f.nome, f.sobrenome, f.email, f.matricula, f.data_efetivacao AS dataAdmissao, c.cargo, d.departamento, f.avatar, f.genero, f.celular, f.telefone, f.data_nasc AS dataNascimento, f.RG AS rg, f.CPF AS cpf, f.salario, f.data_demissao AS dataDemissao, f.id_cargo AS idCargo, f.id_departamento AS idDepartamento, f.id_endereco AS idEndereco, e.logradouro, e.numero, e.bairro, e.cep, e.complemento, ci.id AS idCidade, ci.cidade, es.id AS idEstado, es.estado, es.UF as uf FROM tbl_funcionario AS f INNER JOIN tbl_cargo AS c ON c.id = f.id_cargo INNER JOIN tbl_departamento AS d ON d.id = f.id_departamento INNER JOIN tbl_endereco AS e ON e.id = f.id_endereco INNER JOIN tbl_cidade AS ci ON ci.id = e.id_cidade INNER JOIN tbl_estado AS es ON es.id = ci.id_estado WHERE f.id = ?", [id], callback);
+        this.connection.query("SELECT f.id, f.nome, f.sobrenome, f.email, f.matricula, f.data_efetivacao AS dataAdmissao, c.cargo, d.departamento, f.avatar, f.genero, f.celular, f.telefone, f.data_nasc AS dataNascimento, f.RG AS rg, f.CPF AS cpf, f.salario, f.data_demissao AS dataDemissao, f.id_cargo AS idCargo, f.id_departamento AS idDepartamento, f.id_endereco AS idEndereco, e.logradouro, e.numero, e.bairro, e.cep, e.complemento, ci.id AS idCidade, ci.cidade, es.id AS idEstado, es.estado, es.UF as uf FROM tbl_funcionario AS f INNER JOIN tbl_cargo AS c ON c.id = f.id_cargo INNER JOIN tbl_departamento AS d ON d.id = f.id_departamento INNER JOIN tbl_endereco AS e ON e.id = f.id_endereco INNER JOIN tbl_cidade AS ci ON ci.id = e.id_cidade INNER JOIN tbl_estado AS es ON es.id = ci.id_estado WHERE f.id = ?", [id], (err, result) => {
+            transformResult(result);
+            callback(err, result);
+        });
     }
 
     listar(callback) {
-        this.connection.query("SELECT f.id, f.nome, f.sobrenome, f.email, f.matricula, f.data_efetivacao AS dataAdmissao, c.cargo, d.departamento, f.avatar, f.genero, f.celular, f.telefone, f.data_nasc AS dataNascimento, f.RG AS rg, f.CPF AS cpf, f.salario, f.data_demissao AS dataDemissao, f.id_cargo AS idCargo, f.id_departamento AS idDepartamento, f.id_endereco AS idEndereco, e.logradouro, e.numero, e.bairro, e.cep, e.complemento, ci.id AS idCidade, ci.cidade, es.id AS idEstado, es.estado, es.UF as uf FROM tbl_funcionario AS f INNER JOIN tbl_cargo AS c ON c.id = f.id_cargo INNER JOIN tbl_departamento AS d ON d.id = f.id_departamento INNER JOIN tbl_endereco AS e ON e.id = f.id_endereco INNER JOIN tbl_cidade AS ci ON ci.id = e.id_cidade INNER JOIN tbl_estado AS es ON es.id = ci.id_estado ORDER BY f.id ASC", callback);
+        this.connection.query("SELECT f.id, f.nome, f.sobrenome, f.email, f.matricula, f.data_efetivacao AS dataAdmissao, c.cargo, d.departamento, f.avatar, f.genero, f.celular, f.telefone, f.data_nasc AS dataNascimento, f.RG AS rg, f.CPF AS cpf, f.salario, f.data_demissao AS dataDemissao, f.id_cargo AS idCargo, f.id_departamento AS idDepartamento, f.id_endereco AS idEndereco, e.logradouro, e.numero, e.bairro, e.cep, e.complemento, ci.id AS idCidade, ci.cidade, es.id AS idEstado, es.estado, es.UF as uf FROM tbl_funcionario AS f INNER JOIN tbl_cargo AS c ON c.id = f.id_cargo INNER JOIN tbl_departamento AS d ON d.id = f.id_departamento INNER JOIN tbl_endereco AS e ON e.id = f.id_endereco INNER JOIN tbl_cidade AS ci ON ci.id = e.id_cidade INNER JOIN tbl_estado AS es ON es.id = ci.id_estado ORDER BY f.id ASC", (err, result) => {
+            transformResult(result);
+            callback(err, result);
+        });
     }
 
     inserir(dados, callback) {
@@ -24,3 +30,19 @@ class FuncionarioDAO {
 }
 
 module.exports = () => FuncionarioDAO;
+
+function transformResult(result) {
+    if (result) {
+        result.map(row => {
+            row.endereco = {id: row.idEndereco};
+            delete row["idEndereco"];
+
+            ["cidade", "estado", "id", "logradouro", "idEstado", "idCidade", "complemento", "numero", "cep", "bairro", "uf"].forEach(key => {
+                row.endereco[key] = row[key];
+                delete row[key];
+            });
+
+            return row;
+        });
+    }
+}
