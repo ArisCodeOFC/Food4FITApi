8813d6cbe5410fe425461eadc3c311b42dddc066
WESLEY ALVES DA SILVA <17170129@CFP127ALUNO.LOCAL>
2018-11-22 16:41:55 -0200

    Correção no cadastro de endereço

--- app/database/EnderecoDAO.js
+++ app/database/EnderecoDAO.js
@@ -4,7 +4,7 @@ class EnderecoDAO {
     }
 
     inserir(dados, callback) {
-        this.connection.query("INSERT INTO tbl_endereco (id_cidade, logradouro, numero, bairro, cep, complemento) VALUES (?, ?, ?, ?, ?, ?)", [dados.idCidade, dados.logradouro, dados.numero, dados.bairro, dados.cep, dados.complemento], callback);
+        this.connection.query("INSERT INTO tbl_endereco (id_cidade, logradouro, numero, bairro, cep, complemento) VALUES (?, ?, ?, ?, ?, ?)", [dados.cidade.id, dados.logradouro, dados.numero, dados.bairro, dados.cep, dados.complemento], callback);
     }
 }
 
